b7944570b8eb638a8628af1e15265944f4089013
yasudataiki <y.t.zj4rb@gmail.com>
2020-10-26 11:33:45 +0900

    at workplace , for 1st deploy

--- js/components.js
+++ js/components.js
@@ -4,34 +4,22 @@ Vue.component('navi', {
             <div class="logo"><a href="./"><img src="img/wheat.png">F.F.C</a></div>
             <ul class="g-nav">
                 <li><a href="./">Home</a></li>
+                <li><a href="news.html">News</a></li>
                 <li><a href="terms.html">Works</a></li>
-                <li><a href="terms.html">About</a></li>
-                <li><a href="contact.html">Contact</a></li>
             </ul>
         </div>
     `
   })
-  Vue.component('left', {
+  Vue.component('foot', {
     template: `
-        <div class="advertisement">
-            <h3>相互リンク</h3>
-            <div class="sponcered"><a href="https://www2.popls.co.jp/pop/" target="_blank"><img src="ad_sponcered/popls.jpg" alt="同人誌 各種 印刷 株式会社ポプルス"></a><p>株式会社ポプルス 同人誌 各種 印刷</p></div>
-            <div class="sponcered"><a href="https://www.eikou.com/" target="_blank"><img src="ad_sponcered/eikou.gif" alt="同人誌 印刷｜株式会社 栄光｜栄光情報最前線"></a><p>株式会社 栄光｜同人誌 印刷｜栄光情報最前線</p></div>
-            <div class="sponcered"><a href="https://www.ryokuyou.co.jp/" target="_blank"><img src="ad_sponcered/bn_ryokuyou.jpg" alt="緑陽社 同人誌 グッズ印刷"></a><p>緑陽社 同人誌 グッズ印刷</p></div>
-            <div class="sponcered"><a href="https://www.teis-p.com/" target="_blank"><img src="ad_sponcered/teis-p.jpg" alt="同人誌印刷テイズプリント｜激安高精細印刷"></a><p>同人誌印刷テイズプリント｜激安高精細印刷</p></div>
-            <div class="sponcered"><a href="https://suzunet.co.jp/" target="_blank"><img src="ad_sponcered/suzutou.jpg" alt="スズトウシャドウ印刷 | 同人誌・一般印刷"></a><p>スズトウシャドウ印刷 | 同人誌・一般印刷</p></div>
+        <div class="foot-wrapper">
+            <div class="logo"><a href="./"><img src="img/wheat.png">F.F.C</a></div>
+            <ul class="foot-nav">
+                <li><a href="./">Home</a></li>
+                <li><a href="news.html">News</a></li>
+                <li><a href="terms.html">Works</a></li>
+            </ul>
+            <div class="copyright">2020&copy;FictiousFoodsCompany</div>
         </div>
     `
-  })
-  Vue.component('right', {
-      template: `
-          <div class="advertisement">
-              <div class="sponcered"><a href="https://www.print-walk.co.jp/index.html" target="_blank"><img src="ad_sponcered/print-walk.gif" alt="PrintWalk 同人誌印刷・同人誌・印刷会社・自費出版・チラシの印刷"></a><p>PrintWalk 同人誌印刷・同人誌・印刷会社・自費出版・チラシの印刷</p></div>
-              <div class="sponcered"><a href="https://printking.co.jp/" target="_blank"><img src="ad_sponcered/printking.gif" alt="同人誌印刷通販【プリントキング】"></a><p>プリントキング 同人誌印刷通販</p></div>
-              <div class="sponcered"><a href="https://www.akatsuki-insatsu.co.jp/" target="_blank"><img src="ad_sponcered/akatuki.jpg" alt="あかつき印刷,同人,同人誌,印刷,同人誌印刷"></a><p>あかつき印刷,同人,同人誌,印刷,同人誌印刷</p></div>
-              <div class="sponcered"><a href="https://www.pico-net.com/doujinshi/" target="_blank"><img src="ad_sponcered/pico_bunner.gif" alt="同人誌印刷｜PICO（プリンティングイン株式会社）"></a><p>同人誌印刷｜PICO（プリンティングイン株式会社）</p></div>
-              <div class="sponcered"><a href="https://www.kurieisha.co.jp/" target="_blank"><img src="ad_sponcered/kurieisha_banner.gif" alt="同人誌印刷所くりえい社"></a><p>同人誌印刷所くりえい社</p></div>
-              <a class="twitter-timeline" data-height="450" href="https://twitter.com/aireventinfo?ref_src=twsrc%5Etfw">Tweets by aireventinfo</a>
-          </div>
-      `
   })
