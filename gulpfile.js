21eb52058ba80b101aec4d07c5ff26f99d958f68
DESKTOP-5R07PN4\numb <y.t.zj4rb@gmail.com>
2021-01-03 22:52:27 +0900

    some

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+
+// style.scssをタスクを作成する
+gulp.task("default", function() {
+  // style.scssファイルを取得
+  return (
+    gulp
+      .src("css/style.scss")
+      // Sassのコンパイルを実行
+      .pipe(
+        sass({
+          outputStyle: "expanded"
+        })
+      )
+      // cssフォルダー以下に保存
+      .pipe(gulp.dest("css"))
+  );
+});
--- js/components.js
+++ js/components.js
@@ -1,11 +1,44 @@
 Vue.component('navi', {
     template: `
         <div class="nav-wrapper">
-            <div class="logo"><a href="./"><img src="img/wheat.png">F.F.C</a></div>
+            <div class="logo">
+                <a href="./">
+                    <img src="img/logo.svg">
+                    <span class="pc-display">ボーダーコリー<br>と暮らす</span>
+                </a>
+            </div>
             <ul class="g-nav">
-                <li><a href="./">Home</a></li>
-                <li><a href="news.html">News</a></li>
-                <li><a href="works.html">Works</a></li>
+                <li><a href="./">ホーム</a></li>
+                <li><a href="news.html">健康</a></li>
+                <li><a href="works.html">生活</a></li>
+                <li><a href="works.html">飼う前に</a></li>
+            </ul>
+        </div>
+    `
+  })
+  Vue.component('side', {
+    template: `
+        <div class="side-wrapper pc-display">
+            <ul class="side-inner">
+                <li><a href="news.html">ホーム</a></li>
+                <li><a href="works.html">お知らせ</a></li>
+                <li><a href="works.html">このサイトについて</a></li>
+                <li><a href="works.html">うちの子について</a></li>
+            </ul>
+            <ul class="side-inner">
+                <li><a href="news.html">生活</a></li>
+                <li><a href="works.html">遊び</a></li>
+                <li><a href="works.html">しつけ</a></li>
+            </ul>
+            <ul class="side-inner">
+                <li><a href="news.html">健康</a></li>
+                <li><a href="works.html">怪我・傷</a></li>
+                <li><a href="works.html">体調不良</a></li>
+            </ul>
+            <ul class="side-inner">
+                <li><a href="news.html">飼う前のはなし</a></li>
+                <li><a href="works.html">遊び</a></li>
+                <li><a href="works.html">しつけ</a></li>
             </ul>
         </div>
     `
@@ -13,11 +46,16 @@ Vue.component('navi', {
   Vue.component('foot', {
     template: `
         <div class="foot-wrapper">
-            <div class="logo"><a href="./"><img src="img/wheat.png">F.F.C</a></div>
+            <div class="logo">
+                <a href="./">
+                    <img src="img/logo.svg">
+                    <span class="pc-display">ボーダーコリー<br>と暮らす</span>
+                </a>
+            </div>
             <ul class="foot-nav">
                 <li><a href="./">Home</a></li>
-                <li><a href="news.html">News</a></li>
-                <li><a href="works.html">Works</a></li>
+                <li><a href="news.html">Health</a></li>
+                <li><a href="works.html">Life</a></li>
             </ul>
             <div class="copyright">2020&copy;FictiousFoodsCompany</div>
         </div>
