ef52e30d519cabd1e759f9d1e833d9b46aa9c78e
taiki_yasuda <y.t.zj4rb@gmail.com>
2021-02-17 13:09:53 +0900

    a

--- js/components.js
+++ js/components.js
@@ -9,9 +9,9 @@ Vue.component('navi', {
             </div>
             <ul class="g-nav">
                 <li><a href="./">ホーム</a></li>
-                <li><a href="news.html">健康</a></li>
-                <li><a href="works.html">生活</a></li>
-                <li><a href="works.html">飼う前に</a></li>
+                <li><a href="health.html">健康</a></li>
+                <li><a href="life.html">生活</a></li>
+                <li><a href="ready.html">飼う前に</a></li>
             </ul>
         </div>
     `
@@ -20,30 +20,29 @@ Vue.component('navi', {
     template: `
         <div class="side-wrapper pc-display">
             <ul class="side-inner">
-                <li><a href="news.html">ホーム</a></li>
-                <li><a href="works.html">お知らせ</a></li>
-                <li><a href="works.html">このサイトについて</a></li>
-                <li><a href="works.html">うちの子について</a></li>
+                <li><a href="./">ホーム</a></li>
+                <li><a href="./">お知らせ</a></li>
+                <li><a href="./">このサイトについて</a></li>
+                <li><a href="./">うちの子について</a></li>
             </ul>
             <ul class="side-inner">
-                <li><a href="news.html">ボーダーコリー概要</a></li>
-                <li><a href="works.html">遊び</a></li>
-                <li><a href="works.html">しつけ</a></li>
+                <li><a href="overview.html">ボーダーコリー概要</a></li>
+                <li><a href="overview.html">遊び</a></li>
             </ul>
             <ul class="side-inner">
-                <li><a href="news.html">飼う前のはなし</a></li>
-                <li><a href="works.html">出会い</a></li>
-                <li><a href="works.html">準備</a></li>
+                <li><a href="ready.html">飼う前のはなし</a></li>
+                <li><a href="ready.html">出会い</a></li>
+                <li><a href="ready.html">準備</a></li>
             </ul>
             <ul class="side-inner">
-                <li><a href="news.html">生活</a></li>
-                <li><a href="works.html">遊び</a></li>
-                <li><a href="works.html">しつけ</a></li>
+                <li><a href="life.html">生活</a></li>
+                <li><a href="life.html">遊び</a></li>
+                <li><a href="life.html">しつけ</a></li>
             </ul>
             <ul class="side-inner">
-                <li><a href="news.html">健康</a></li>
-                <li><a href="works.html">怪我・傷</a></li>
-                <li><a href="works.html">体調不良</a></li>
+                <li><a href="health.html">健康</a></li>
+                <li><a href="health.html">怪我・傷</a></li>
+                <li><a href="health.html">体調不良</a></li>
             </ul>
         </div>
     `
@@ -59,9 +58,9 @@ Vue.component('navi', {
             </div>
             <ul class="foot-nav">
                 <li><a href="./">ホーム</a></li>
-                <li><a href="news.html">健康</a></li>
-                <li><a href="works.html">生活</a></li>
-                <li><a href="works.html">飼う前に</a></li>
+                <li><a href="health.html">健康</a></li>
+                <li><a href="life.html">生活</a></li>
+                <li><a href="ready.html">飼う前に</a></li>
             </ul>
             <div class="copyright">&copy;201089 安田大輝</div>
         </div>
