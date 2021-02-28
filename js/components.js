e5bd06043bfe52d804fbfebab262b4e16025e925
DESKTOP-5R07PN4\numb <y.t.zj4rb@gmail.com>
2021-02-28 20:54:46 +0900

    af

--- js/components.js
+++ js/components.js
@@ -21,28 +21,29 @@ Vue.component('navi', {
         <div class="side-wrapper pc-display">
             <ul class="side-inner">
                 <li><a href="./">ホーム</a></li>
-                <li><a href="./">お知らせ</a></li>
-                <li><a href="./">このサイトについて</a></li>
-                <li><a href="./">うちの子について</a></li>
+                <li><a href="./#01">お知らせ</a></li>
+                <li><a href="./#02">このサイトについて</a></li>
+                <li><a href="./#03">うちの子について</a></li>
             </ul>
             <ul class="side-inner">
                 <li><a href="overview.html">ボーダーコリー概要</a></li>
-                <li><a href="overview.html">遊び</a></li>
+                <li><a href="overview.html#01">一般的なボーダーコリー</a></li>
             </ul>
             <ul class="side-inner">
-                <li><a href="ready.html">飼う前のはなし</a></li>
-                <li><a href="ready.html">出会い</a></li>
-                <li><a href="ready.html">準備</a></li>
+                <li><a href="ready.html">飼う前に</a></li>
+                <li><a href="ready.html#01">出会い</a></li>
+                <li><a href="ready.html#02">準備</a></li>
             </ul>
             <ul class="side-inner">
                 <li><a href="life.html">生活</a></li>
-                <li><a href="life.html">遊び</a></li>
-                <li><a href="life.html">しつけ</a></li>
+                <li><a href="life.html#01">ご飯</a></li>
+                <li><a href="life.html#02">しつけ</a></li>
+                <li><a href="life.html#03">遊び</a></li>
             </ul>
             <ul class="side-inner">
                 <li><a href="health.html">健康</a></li>
-                <li><a href="health.html">怪我・傷</a></li>
-                <li><a href="health.html">体調不良</a></li>
+                <li><a href="health.html#01">病気・ワクチン</a></li>
+                <li><a href="health.html#02">怪我・傷</a></li>
             </ul>
         </div>
     `
